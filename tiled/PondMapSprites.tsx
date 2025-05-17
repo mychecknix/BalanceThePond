0aed457461a8fe24264fbcb2a14f347710edddc4
Michael Grüb <grueb.m@googlemail.com>
2025-05-17 12:17:02 +0200

    a

---
+++ tiled/PondMapSprites.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="PondMapSprites" tilewidth="16" tileheight="16" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="sprites/pond-edge-horizontal.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="sprites/pond-edge-horizontal-1.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="sprites/Steg.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="sprites/Sprite-Pond Welle li.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="sprites/Sprite-Pond Welle re.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="sprites/Sprite-Rand.png" width="16" height="16"/>
+ </tile>
+</tileset>
